f2bfee9776e97960ea7dc0d2d3c78915dc63e8c4
barcanerkoc <barcanerkoc@hotmail.com>
2016-10-12 14:28:07 +0300

    Master

---
+++ JavaApplication6/src/javaapplication6/CalculatorJavaApplication6.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author baris
+ */
+public class CalculatorJavaApplication6 {
+    
+    
+    public static void main(String[] args) {
+        
+        int a = 5, b = 3, func;
+        Scanner bar = new Scanner(System.in);
+        
+        System.out.println("Toplama için 0 giriniz\n"
+                + "Çikarma için 1 giriniz\n"
+                + "Çarpma için 2 giriniz\n"
+                + "Bölme için 3 giriniz");
+        
+        func = bar.nextInt();
+        
+        if(func == 0){
+            //toplama
+        }else if(func == 1){
+            //çikarma
+        }else if(func == 2){
+            //çarpma
+        }else if(func == 3){
+            //bölme
+        }else{
+            System.out.println("Hatali islem");
+        }
+        
+    }
+    
+}
